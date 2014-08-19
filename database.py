dc446d5872a69af81a498f7265748130e7ace134
rbloomdale <rbloomdale@zynga.com>
2014-08-19 14:23:51 -0700

    trying to get relationships to work

--- database.py
+++ database.py
@@ -258,7 +258,7 @@ class RecordsMediTrainV2Survey(db.Model):
     question2 = db.Column(db.Integer())
     question3 = db.Column(db.Integer())
     timestamp = db.Column(db.Integer())
-#    session_id = db.Column(db.Integer(), db.ForeignKey(RecordsMediTrainV2Session.__tablename__ + ".id"))
+    session_id = db.Column(db.Integer(), db.ForeignKey("session.id"))
     subjectID = db.Column(db.String(50))
     sessionID = db.Column(db.String(55))
 
@@ -296,7 +296,7 @@ class RecordsMediTrainV2MiniSession(db.Model):
     endTime = db.Column(db.Integer())
     result = db.Column(db.Integer())
     startTime = db.Column(db.Integer())
-#    session_id = db.Column(db.Integer(), db.ForeignKey(RecordsMediTrainV2Session.__tablename__ + ".id"))
+    session_id = db.Column(db.Integer(), db.ForeignKey("session.id"))
     sessionID = db.Column(db.String(55))
     subjectID = db.Column(db.String(50))
 
