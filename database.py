6e6e8d2079dd4406993726c9545005d181afb234
rbloomdale <rbloomdale@zynga.com>
2014-08-20 10:10:23 -0700

    keyword args

--- database.py
+++ database.py
@@ -287,9 +287,9 @@ class RecordsMediTrainV2Session(db.Model):
         self.miniSessionCount = miniSessionCount
         log.debug("Meditrain V2 Session ID=%r miniSessions=%r",sessionID ,miniSessions)
         for miniSession in miniSessions:
-            self.miniSessions.append(RecordsMediTrainV2MiniSession(miniSession))
+            self.miniSessions.append(RecordsMediTrainV2MiniSession(**miniSession))
         if(self.survey != None):
-            self.survey = RecordsMediTrainV2Survey(survey)
+            self.survey = RecordsMediTrainV2Survey(**survey)
 
 #class RecordsMediTrainV2Subject(db.Model):
 #    study_ID = 'testMeditrain'
